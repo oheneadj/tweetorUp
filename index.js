8652028772b8c92e9d45700205015804c44caa0f
oheneadj <oheneadjei.dev@gmail.com>
2023-02-06 03:51:10 +0000

    added twitter client and cron

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 const { twitterClient } = require("./twitterClient.js");
 const CronJob = require("cron").CronJob;
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
 const tweet = async () => {
   try {
-    await twitterClient.v2.tweet("Hello world!");
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+        "X-RapidAPI-Host": process.env.RAPID_API_HOST,
+      },
+    };
+
+    fetch(process.env.API_URL, options)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response[0].fact);
+        twitterClient.v2.tweet(response[0].fact);
+        return response[0].fact;
+      });
   } catch (e) {
     console.log(e);
   }
 };
 
-const cronTweet = new CronJob("0 */4 * * *", async () => {
+const cronTweet = new CronJob("* * * * *", async () => {
   tweet();
 });
 
